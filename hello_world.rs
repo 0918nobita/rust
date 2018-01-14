4e8c26dda7f36b44d9175da9fe88f8706ef76e94
0918nobita <nobita.0918@gmail.com>
2018-01-15 07:50:54 +0900

    Fix typo

---
+++ hello_world.rs
@@ -0,0 +1,9 @@
+// println!(...) はマクロを呼び出している
+// "Hello, world!" は静的に確保された文字列
+
+fn main() {
+	println!("Hello, world!");
+}
+
+// Cargo は Rust のビルドシステムであり、パッケージマネージャであり
+// Rustacean は Rust プロジェクトの管理にも使う
