fd71f61e94b4c48e30f7377b647b7c31faa43520
0918nobita <nobita.0918@gmail.com>
2018-03-13 21:43:55 +0900

    Cargo.lockを管理対象とした

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,17 @@
+// io 標準ライブラリを読み込む
 use std::io;
 
+// エントリーポイント
+// 戻り値の型は書いていないため、空のタプルとして扱われる
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
+
+    // 変数束縛を作る ( mut を使用してミュータブルに )
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    
+    io::stdin().read_line(&mut guess)
+    	.expect("Failed to read line");
+    
     println!("You guessed: {}", guess);
 }
