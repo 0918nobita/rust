b5256ed474fd51c0334b9da542bd12dd69370b3c
0918nobita <nobita.0918@gmail.com>
2018-07-05 08:40:56 +0900

    rand クレートを読み込むサンプルを追加した

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,17 +1,27 @@
+extern crate rand;
+
 // io 標準ライブラリを読み込む
 use std::io;
+use rand::Rng;
 
 // エントリーポイント
 // 戻り値の型は書いていないため、空のタプルとして扱われる
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     // 変数束縛を作る ( mut を使用してミュータブルに )
     let mut guess = String::new();
     
-    io::stdin().read_line(&mut guess)
+    io::stdin().read_line(&mut guess)  // &mut guess は guess の参照
     	.expect("Failed to read line");
     
     println!("You guessed: {}", guess);
 }
+
+// Rust コードのパッケージを「クレート」という。
