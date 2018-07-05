f863a21599771c61851758d3894f9839a6927ec9
0918nobita <nobita.0918@gmail.com>
2018-07-05 09:03:30 +0900

    loop を用いたサンプルを追加した

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -15,26 +15,31 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // 変数束縛を作る ( mut を使用してミュータブルに )
-    let mut guess = String::new();
+        // 変数束縛を作る ( mut を使用してミュータブルに )
+        let mut guess = String::new();
     
-    io::stdin().read_line(&mut guess)  // &mut guess は guess の参照
-    	.expect("Failed to read line");
+        io::stdin().read_line(&mut guess)  // &mut guess は guess の参照
+    	    .expect("Failed to read line");
 
-    // すでに定義してある guess を新しい定義で隠す ( シャドーイング )
-    // 文字列の trim メソッドは、文字列の最初と最後にある空白を取り除く
-    // 文字列の parse メソッドは、文字列を何かの数値へとパースする
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number");
+        // すでに定義してある guess を新しい定義で隠す ( シャドーイング )
+        // 文字列の trim メソッドは、文字列の最初と最後にある空白を取り除く
+        // 文字列の parse メソッドは、文字列を何かの数値へとパースする
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number");
     
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    // cmp() 自分と比較可能なものの参照を受け取り、Ordering を返す
-    match guess.cmp(&secret_number) {
-        Ordering::Less    => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal   => println!("You Win!")
+        // cmp() 自分と比較可能なものの参照を受け取り、Ordering を返す
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You Win!");
+                break;
+            }
+        }
     }
 }
