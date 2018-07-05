8fd0028c94f5d790c6e1e98491868150d2720d1c
0918nobita <nobita.0918@gmail.com>
2018-07-05 09:00:19 +0900

    シャドーイングを用いたサンプルを追加した

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,7 @@ extern crate rand;
 
 // io 標準ライブラリを読み込む
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 // エントリーポイント
@@ -9,6 +10,7 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
+    // rand::thread_rng() スレッドにローカルな乱数生成器のコピーを取得する
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("The secret number is: {}", secret_number);
@@ -20,8 +22,19 @@ fn main() {
     
     io::stdin().read_line(&mut guess)  // &mut guess は guess の参照
     	.expect("Failed to read line");
+
+    // すでに定義してある guess を新しい定義で隠す ( シャドーイング )
+    // 文字列の trim メソッドは、文字列の最初と最後にある空白を取り除く
+    // 文字列の parse メソッドは、文字列を何かの数値へとパースする
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number");
     
     println!("You guessed: {}", guess);
-}
 
-// Rust コードのパッケージを「クレート」という。
+    // cmp() 自分と比較可能なものの参照を受け取り、Ordering を返す
+    match guess.cmp(&secret_number) {
+        Ordering::Less    => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal   => println!("You Win!")
+    }
+}
