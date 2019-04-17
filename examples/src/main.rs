c0c2ab6d3735dc85776cda5ad84b98be890ab0e4
0918nobita <nobita.0918@gmail.com>
2019-04-17 11:00:45 +0900

    :sparkles: struct を用いたサンプルを追加した

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,5 +1,18 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+// struct が持つ値はイミュータブルがデフォルト
+// mut を用いるとミュータブルに変更できる
+// ただし、ミュータビリティは束縛にのみ付与できる属性であり、
+// 構造体の定義に付与できる属性ではない
+
 fn main() {
     let v1 = vec![1, 2, 3];
     let v2 = &v1;
     println!("{}, {}", v1[0], v2[1]);
+
+    let origin = Point { x: 0, y: 0 };
+    println!("The origin is at ({}, {})", origin.x, origin.y);
 }
