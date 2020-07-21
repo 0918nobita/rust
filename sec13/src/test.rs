534b9d75b3f7a3295596c27040b95d2aeead6233
0918nobita <nobita.0918@gmail.com>
2020-07-21 19:23:25 +0900

    :white_check_mark: イテレータに関するテストを追加

--- sec13/src/test.rs
+++ sec13/src/test.rs
@@ -47,3 +47,33 @@ where
 {
     f(arg)
 }
+
+#[test]
+fn iter_demo() {
+    let v1 = vec![1, 2, 3];
+    let mut v1_iter = v1.iter();
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+
+    let mut v1_iter2 = v1.into_iter();
+    assert_eq!(v1_iter2.next(), Some(1));
+    assert_eq!(v1_iter2.next(), Some(2));
+    assert_eq!(v1_iter2.next(), Some(3));
+    assert_eq!(v1_iter2.next(), None);
+}
+
+#[test]
+fn iter_sum() {
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+    let total: i32 = v1_iter.sum(); // sum: consuming adaptor
+    assert_eq!(total, 6);
+
+    let v1_iter = v1.iter();
+    let v2: Vec<_> = v1_iter
+        .map(|x| x + 1) // map: iterator adaptor
+        .collect(); // collect: consuming adaptor
+    assert_eq!(v2, vec![2, 3, 4]);
+}
