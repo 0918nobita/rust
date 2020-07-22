722fe85567d0d766cd0d67cb3145232500e1d079
0918nobita <nobita.0918@gmail.com>
2020-07-22 11:03:22 +0900

    :white_check_mark: 独自のイテレータに関するテストを追加

--- sec13/src/test.rs
+++ sec13/src/test.rs
@@ -91,15 +91,76 @@ fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
 #[test]
 fn filter_by_size() {
     let shoes = vec![
-        Shoe { size: 10, style: String::from("sneaker") },
-        Shoe { size: 13, style: String::from("sandal") },
-        Shoe { size: 10, style: String::from("boot") },
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 13,
+            style: String::from("sandal"),
+        },
+        Shoe {
+            size: 10,
+            style: String::from("boot"),
+        },
     ];
 
     let in_my_size = shoes_in_my_size(shoes, 10);
 
-    assert_eq!(in_my_size, vec![
-        Shoe { size: 10, style: String::from("sneaker") },
-        Shoe { size: 10, style: String::from("boot") },
-    ]);
+    assert_eq!(
+        in_my_size,
+        vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker")
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot")
+            },
+        ]
+    );
+}
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Self {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+    for i in 0..5 {
+        assert_eq!(counter.next(), Some(i + 1))
+    }
+    assert_eq!(counter.next(), None)
+}
+
+#[test]
+fn using_other_iterator_trait_methods() {
+    let sum: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+    assert_eq!(sum, 18);
 }
