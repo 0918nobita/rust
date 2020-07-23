1f36da8bf564db216514eb5034d9034c2a2636b4
0918nobita <nobita.0918@gmail.com>
2020-07-23 15:27:07 +0900

    :sparkles: 循環参照に関するサンプルを追加

--- sec15/src/main.rs
+++ sec15/src/main.rs
@@ -1,3 +1,4 @@
+use std::cell::RefCell;
 use std::rc::Rc;
 
 extern crate sec15;
@@ -6,6 +7,7 @@ use sec15::{list_rc, mybox::MyBox};
 fn main() {
     drop_mybox();
     ref_count();
+    circular_ref();
 }
 
 fn drop_mybox() {
@@ -44,4 +46,41 @@ fn ref_count() {
     }
 
     println!("Ref count: {}", Rc::strong_count(&a));
+}
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match *self {
+            Cons(_, ref item) => Some(item),
+            Nil => None,
+        }
+    }
+}
+
+fn circular_ref() {
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+    println!("a initial ref count: {}", Rc::strong_count(&a)); // 1
+    println!("a next item: {:?}", a.tail().unwrap()); // RefCell { value: Nil }
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+    println!("a ref count after b creation: {}", Rc::strong_count(&a)); // 2
+    println!("b initial ref count: {}", Rc::strong_count(&b)); // 1
+    println!("b next item: {:?}", b.tail().unwrap()); // RefCell { value: Cons(5, RefCell { value: Nil }) }
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b ref count after changing: {}", Rc::strong_count(&b)); // 2
+    println!("a ref count after changing: {}", Rc::strong_count(&a)); // 2
+
+    // println!("{:?}", a.tail()); // stack overflow
 }
