89729d9aca3a3ee3f34d7d22423daab8838356be
0918nobita <nobita.0918@gmail.com>
2020-07-23 21:24:44 +0900

    :sparkles: Rc<T>, Weak<T> に関するサンプルを追加

--- sec15/src/main.rs
+++ sec15/src/main.rs
@@ -1,5 +1,5 @@
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 extern crate sec15;
 use sec15::{list_rc, mybox::MyBox};
@@ -8,6 +8,8 @@ fn main() {
     drop_mybox();
     ref_count();
     circular_ref();
+    tree();
+    tree_weak();
 }
 
 fn drop_mybox() {
@@ -83,4 +85,67 @@ fn circular_ref() {
     println!("a ref count after changing: {}", Rc::strong_count(&a)); // 2
 
     // println!("{:?}", a.tail()); // stack overflow
+}
+
+fn tree() {
+    #[derive(Debug)]
+    struct Node {
+        value: i32,
+        children: RefCell<Vec<Rc<Node>>>,
+    }
+
+    let leaf = Rc::new(Node {
+        value: 3,
+        children: RefCell::new(vec![]),
+    });
+    let branch = Rc::new(Node {
+        value: 5,
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+    println!("{:?}", branch);
+}
+
+fn tree_weak() {
+    #[derive(Debug)]
+    struct Node {
+        value: i32,
+        parent: RefCell<Weak<Node>>,
+        children: RefCell<Vec<Rc<Node>>>,
+    }
+
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+    println!(
+        "[1] leaf   strong = {}, weak = {}",
+        Rc::strong_count(&leaf), // 1
+        Rc::weak_count(&leaf)    // 0
+    );
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+        println!("[2] {:?}", leaf.parent.borrow().upgrade()); // None
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+        println!(
+            "[3] leaf   strong = {}, weak = {}",
+            Rc::strong_count(&leaf), // 2
+            Rc::weak_count(&leaf)    // 0
+        );
+        println!(
+            "[4] branch strong = {}, weak = {}",
+            Rc::strong_count(&branch), // 1
+            Rc::weak_count(&branch)    // 1
+        );
+        println!("[5] {:?}", leaf.parent.borrow().upgrade()); // Some({*branch})
+    }
+    println!(
+        "[6] leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf), // 1
+        Rc::weak_count(&leaf)    // 0
+    );
 }
