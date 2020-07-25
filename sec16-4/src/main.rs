115106e213f7b50e39b505a168bb8ee189c59f38
0918nobita <nobita.0918@gmail.com>
2020-07-25 13:14:04 +0900

    :sparkles: mpsc を用いたサンプルを追加

---
+++ sec16-4/src/main.rs
@@ -0,0 +1,40 @@
+use std::sync::mpsc::{self, Sender};
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let (tx1, rx) = mpsc::channel();
+    let tx2 = Sender::clone(&tx1);
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx2.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
