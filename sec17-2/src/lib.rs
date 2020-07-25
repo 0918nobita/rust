6c65de872b49adf13cc0f576fe3ac8cd4dc1866c
0918nobita <nobita.0918@gmail.com>
2020-07-25 14:46:54 +0900

    :sparkles: トレイトオブジェクトに関するサンプルを追加

---
+++ sec17-2/src/lib.rs
@@ -0,0 +1,41 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for comp in self.components.iter() {
+            comp.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // 実際にボタンを描画するコード
+        println!("Button");
+    }
+}
+
+pub struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // 実際にセレクトボックスを描画するコード
+        println!("SelectBox {:?}", self.options);
+    }
+}
---
+++ sec17-2/src/main.rs
@@ -0,0 +1,24 @@
+extern crate sec17_2;
+use sec17_2::{Button, Screen, SelectBox};
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+    screen.run();
+}
