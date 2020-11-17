ea89464098a03a765af0702a0cc1b5867c4ed3ce
0918nobita <nobita.0918@gmail.com>
2020-11-17 18:06:30 +0900

    :sparkles: .await, join! を用いたサンプルを追加

--- asynchronous/src/main.rs
+++ asynchronous/src/main.rs
@@ -1,10 +1,26 @@
 use futures::executor::block_on;
+use futures::join;
 
-async fn hello_world() {
-    println!("Hello, world!");
+async fn learn_song() {
+    println!("learn song");
+}
+
+async fn sing_song() {
+    learn_song().await;
+    println!("sing song");
+}
+
+async fn dance() {
+    println!("dance");
+}
+
+async fn async_main() {
+    println!("async main");
+    let f1 = sing_song();
+    let f2 = dance();
+    join!(f1, f2);
 }
 
 fn main() {
-    let future = hello_world();
-    block_on(future);
+    block_on(async_main());
 }
